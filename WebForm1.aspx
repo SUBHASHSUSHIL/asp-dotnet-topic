650e0aa1a1b6b8569788a132adb5ba73a8985c94
Sushil Thakur <160758341+SUBHASHSUSHIL@users.noreply.github.com>
2024-03-23 01:34:28 +0530

    ASP .Net Code

---
+++ WebForm1.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="asp.net_CRUD_operation.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table border="1">
+                <tr>
+                    <th colspan="2">CRUD</th>
+                </tr>
+                <tr>
+                    <th>Id:</th>
+                    <td>
+                        <asp:TextBox ID="txtid" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Name:</th>
+                    <td>
+                        <asp:TextBox ID="txtname" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Email:</th>
+                    <td>
+                        <asp:TextBox ID="txtemail" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Phone:</th>
+                    <td>
+                        <asp:TextBox ID="txtphone" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Address</th>
+                    <td>
+                        <asp:TextBox ID="txtaddress" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <th>
+                        <asp:Button ID="btnsubmit" runat="server" Text="Submit" OnClick="btnsubmit_Click" />
+                        <asp:Button ID="txtupdate" runat="server" Text="Update" OnClick="txtupdate_Click" />
+                        <asp:Button ID="txtdelete" runat="server" Text="Delete" OnClick="txtdelete_Click" />
+                        <asp:Button ID="txtselect" runat="server" Text="Select" OnClick="txtselect_Click" />
+
+                    </th>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace asp.net_CRUD_operation
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString);
+        SqlCommand cmd;
+        public string Query { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Query = "_insertEmp";
+                using (cmd = new SqlCommand(Query, con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtemail.Text);
+                    cmd.Parameters.AddWithValue("@Phone", txtphone.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtaddress.Text);
+                    if (con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    Response.Write("<script>alert('Inserted Successfully..')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        protected void txtupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Query = "_UpdateEmp";
+                using(cmd = new SqlCommand(Query, con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", txtid.Text);
+                    cmd.Parameters.AddWithValue("@Name", txtname.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtemail.Text);
+                    cmd.Parameters.AddWithValue("@Phone", txtphone.Text);
+                    cmd.Parameters.AddWithValue("@Address", txtaddress.Text);
+                    if(con.State == ConnectionState.Closed)
+                    {
+                        con.Open() ;
+                    }
+                    cmd.ExecuteNonQuery();
+                    Response.Write("<script>alert('Updated Successfully..')</script>");
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if(con.State == ConnectionState.Open)
+                {
+                    con.Close() ;
+                }
+            }
+        }
+
+        protected void txtdelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Query = "_deleteEmp";
+                using (cmd = new SqlCommand(Query, con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", txtid.Text);
+                    if(con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    cmd.ExecuteNonQuery();
+                    Response.Write("<script>alert('Deleted Successfully..')</script>");
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                if(con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            
+        }
+
+        protected void txtselect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Query = "_selectEmp";
+                using(cmd = new SqlCommand(Query, con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Id", txtid.Text);
+                    if(con.State == ConnectionState.Closed)
+                    {
+                        con.Open();
+                    }
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if(dr.Read())
+                    {
+                        txtname.Text = dr["Name"].ToString();
+                        txtemail.Text = dr["Email"].ToString();
+                        txtphone.Text = dr["Phone"].ToString() ;
+                        txtaddress.Text = dr["Address"].ToString();
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('No Record Found..')</script>");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                if(con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+        }
+    }
+}
